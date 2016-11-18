a840f4096fc4e075e063ddedf2c077067eed41d6
hwff <weifenghuang2016@u.northwestern.edu>
2016-11-17 19:21:11 -0600

    feeling lucky

--- GETOUT/app/detail/scripts/DetailEventController.js
+++ GETOUT/app/detail/scripts/DetailEventController.js
@@ -1,12 +1,13 @@
 angular
 .module('detail')
   .controller('DetailEventController', function ($scope, $window, supersonic) {
-    var stopListening = supersonic.ui.views.current.params.onValue(function (params) {
-        $scope.currentEvent = params;
+      var stopListening = supersonic.ui.views.current.params.onValue(function (params) {
+          $scope.currentEvent = params;
 
-        $scope.lat = parseFloat($scope.currentEvent.lat);
-        $scope.lng = parseFloat($scope.currentEvent.lng);
+          $scope.lat = parseFloat($scope.currentEvent.lat);
+          $scope.lng = parseFloat($scope.currentEvent.lng);
 
-    });
-    stopListening();
+      });
+      stopListening();
+
   });
